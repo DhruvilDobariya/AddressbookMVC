ceb4aad07043fcf2397369eee940a13acad030ca
Dhruvil Dobariya <190540107051@darshan.ac.in>
2022-03-16 11:26:24 +0530

    City crud complate

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -1,4 +1,5 @@
-﻿using Addressbook.Models;
+﻿using Addressbook.GenericRepository;
+using Addressbook.Models;
 using Addressbook.Repository;
 using Addressbook.Service;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,13 @@ namespace Addressbook.Controllers
     {
         private readonly ICityRepository _CityRepository;
         private readonly IDropDownList _DropDownList;
-        public CityController(ICityRepository CityRepository, IDropDownList DropDownList)
+        private readonly ICRUDRepository<City> _CRUDRepository;
+
+        public CityController(ICityRepository CityRepository, IDropDownList DropDownList, ICRUDRepository<City> CRUDRepository)
         {
             _CityRepository = CityRepository;
             _DropDownList = DropDownList;
+            _CRUDRepository = CRUDRepository;
         }
         public IActionResult Index()
         {
@@ -25,6 +29,73 @@ namespace Addressbook.Controllers
         {
             ViewBag.States = _DropDownList.States();
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                if(await _CRUDRepository.InsertAsync(city))
+                {
+                    ModelState.Clear();
+                    TempData["Success"] = "City added successfully";
+                    return View();
+                }
+            }
+            TempData["Error"] = _CRUDRepository.Message;
+            ViewBag.States = _DropDownList.States();
+            return View(city);
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            if(id == 0)
+            {
+                return NotFound("City Not Found");
+            }
+            City city = await _CRUDRepository.GetByIdAsync(id);
+            if(city == null)
+            {
+                TempData["Error"] = _CRUDRepository.Message;
+                return RedirectToAction("Index");
+            }
+            ViewBag.States = _DropDownList.States();
+            return View(city);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                if(await _CRUDRepository.UpdateAsync(city))
+                {
+                    ModelState.Clear();
+                    TempData["Success"] = "City updated successfully";
+                    return RedirectToAction("Index");
+                }
+            }
+            TempData["Error"] = _CRUDRepository.Message;
+            ViewBag.States = _DropDownList.States();
+            return View(city);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if(id == 0)
+            {
+                return NotFound("City Not Found");
+            }
+            if(await _CRUDRepository.DeleteAsync(id))
+            {
+                TempData["Success"] = "City deleted successfully";
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = _CRUDRepository.Message;
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/City.cs
+++ Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Addressbook.Models
 {
@@ -10,14 +11,21 @@ namespace Addressbook.Models
             Contacts = new HashSet<Contact>();
         }
 
+        [Key]
         public int CityId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter City Name")]
+        [Display(Name = "City Name")]
         public string CityName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please Select State")]
+        [Display(Name = "State Name")]
         public int StateId { get; set; }
         public string? Stdcode { get; set; }
         public string? PinCode { get; set; }
         public DateTime? CreationDate { get; set; }
 
-        public virtual State State { get; set; } = null!;
+        public virtual State? State { get; set; }
         public virtual ICollection<Contact> Contacts { get; set; }
     }
 }
